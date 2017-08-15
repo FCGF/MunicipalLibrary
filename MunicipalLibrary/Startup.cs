23672a0c221dfb1b1c320242d669cba637c2bff0
Fernando Cardoso Garcia Filho <fernando061312@gmail.com>
2017-08-15 20:24:58 -0300

    Add project files.

---
+++ MunicipalLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MunicipalLibrary.Startup))]
+namespace MunicipalLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
